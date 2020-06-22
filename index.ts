78149bccd678047952808edd39a33e4ee1a7021e
Nick <nick.richardson@rezonence.com>
2020-06-22 14:57:59 +0100

    Add basic view

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import http from "http";
+
 const app = express();
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.static(__dirname + '/public'));
 
-const server = app.listen(5000);
+const server: http.Server = app.listen(5000);
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 })
