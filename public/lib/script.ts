3384d2e8f3fc287858565f1510053fa9cc4b1c24
Nick <nick.richardson@rezonence.com>
2020-06-22 15:22:19 +0100

    Instantiate socket and capture text

--- public/lib/script.ts
+++ public/lib/script.ts
@@ -1,7 +1,10 @@
+import io from "socket.io";
+
 const SR = window.SpeechRecognition || (window as any).webkitSpeechRecognition;
 const recognition = new SR();
 recognition.lang = 'en-GB';
 recognition.interimResults = false;
+const socket: io.Server = io();
 
 document.querySelector("button")!.addEventListener("click", () => {
     recognition.start();
@@ -10,6 +13,7 @@ document.querySelector("button")!.addEventListener("click", () => {
 recognition.addEventListener('result', (e) => {
     let last = e.results.length - 1;
     let text = e.results[last][0].transcript;
-
+    socket.emit('chat message', text);
     console.log('Confidence: ' + e.results[0][0].confidence);
 });
+
