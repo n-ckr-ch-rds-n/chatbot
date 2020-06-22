3af6adf8eb0b7bb3cb646eeb045d03f9e9e480b2
Nick <nick.richardson@rezonence.com>
2020-06-22 14:42:12 +0100

    Set up server

---
+++ index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express();
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.static(__dirname + '/public'));
+
+const server = app.listen(5000);
+app.get('/', (req, res) => {
+    res.sendFile('index.html');
+})
