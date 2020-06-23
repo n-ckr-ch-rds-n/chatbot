71efacbdad8fdc8dbd8a45f6b2337bf30257e1ea
Nick <nick.richardson@rezonence.com>
2020-06-23 15:46:50 +0100

    Implement voice synthesiser class

--- public/lib/index.ts
+++ public/lib/index.ts
@@ -1,14 +1,16 @@
 import io from 'socket.io-client';
 import {SpeechRecognitionService} from "./speech-recognition-service/speech-recognition.service";
 import {SocketService} from "./socket-service/socket-service";
+import {VoiceSynthesiser} from "./voice-synthesiser/voice-synthesiser";
 
 const SR = window.SpeechRecognition || (window as any).webkitSpeechRecognition;
 const recognition = new SR();
 recognition.lang = 'en-GB';
 recognition.interimResults = false;
 
-const socketService = new SocketService(io());
-const speechRecognitionService = new SpeechRecognitionService(recognition, socketService);
+const voiceSynth = new VoiceSynthesiser(window.speechSynthesis);
+const socketService = new SocketService(io(), voiceSynth);
+const recognitionService = new SpeechRecognitionService(recognition, socketService);
 
 
 // document.querySelector("button")!.addEventListener("click", () => {
@@ -22,15 +24,15 @@ const speechRecognitionService = new SpeechRecognitionService(recognition, socke
 //     console.log('Confidence: ' + e.results[0][0].confidence);
 // });
 
-function synthVoice(text: string) {
-    const synth = window.speechSynthesis;
-    const utterance = new SpeechSynthesisUtterance();
-    utterance.text = text;
-    synth.speak(utterance);
-}
-
-socket.on("bot reply", (replyText: string) => {
-    synthVoice(replyText);
-    console.log("Reply text", replyText);
-});
+// function synthVoice(text: string) {
+//     const synth = window.speechSynthesis;
+//     const utterance = new SpeechSynthesisUtterance();
+//     utterance.text = text;
+//     synth.speak(utterance);
+// }
+//
+// socket.on("bot reply", (replyText: string) => {
+//     synthVoice(replyText);
+//     console.log("Reply text", replyText);
+// });
 
--- public/lib/socket-service/socket-service.ts
+++ public/lib/socket-service/socket-service.ts
@@ -1,11 +1,19 @@
 import Socket = SocketIOClient.Socket;
+import {VoiceSynthesiser} from "../voice-synthesiser/voice-synthesiser";
 
 export class SocketService {
 
-    constructor(private socket: Socket) {
+    constructor(private socket: Socket,
+                private voiceSynth: VoiceSynthesiser) {
+        this.socket.on("bot reply", (text: string) => this.handleReply(text))
     }
 
     emitMessage(text: string) {
         this.socket.emit("chat message", text);
+    }
+
+    handleReply(text: string) {
+        this.voiceSynth.speak(text);
+        console.log("Reply text", text);
     }
 }
